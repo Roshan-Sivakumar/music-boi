7462ca29f82450bea88a96a9a2ded789d584bbd6
Roshan <65855350+Roshan-Sivakumar@users.noreply.github.com>
2022-04-29 11:34:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+bruh = "";
+songftw = "";
+leftwristy = "";
+rightwristy = "";
+
+function setup() {
+    canvas = createCanvas(600, 530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded)
+    posenet.on("pose", gotResult)
+}
+
+function preload() {
+    bruh = loadSound("bruh.mp3");
+    songftw = loadSound("songforthewin.mp3");
+}
+
+function draw() {
+    image(video, 0, 0, 600, 530);
+}
+
+function modelLoaded(){
+    console.log("w");
+}
+
+function gotResult(results){
+    if(results.length > 0){
+        leftwristy = results[0].pose.leftWrist.y
+        rightwristy = results[0].pose.rightWrist.y
+    } else {
+        console.log("DATA not found: array, results")
+    }
+}
